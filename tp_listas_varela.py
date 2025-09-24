61d5c0886910735c1d07e7c95e00278c16327974
Nahuel Varela <nahuelvarelaprogutn@gmail.com>
2025-09-24 15:26:53 -0300

    Inicio tp listas

---
+++ tp_listas_varela.py
@@ -0,0 +1,26 @@
+#1) Crear una lista con los números del 1 al 100 que sean múltiplos de 4. Utilizar la función 
+#range. 
+
+multiplos_de_4 = list(range(4, 101, 4))
+
+print(multiplos_de_4)
+
+#2) Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el penúltimo. ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el indexing con números negativos!
+
+mis_favoritos = ["Play 5", "Café", "Counter", "Futbol", "BOCA"]
+
+
+print("El penúltimo elemento es:", mis_favoritos[-2])
+
+#3) Crear una lista vacía, agregar tres palabras con append e imprimir la lista resultante por 
+#pantalla. Pista: para crear una lista vacía debes colocar los corchetes sin nada en su interior. Por 
+#ejemplo: 
+
+vacia = []
+
+vacia.append("Programar")
+vacia.append("Café")
+vacia.append("Futbol")
+
+print("La lista resultante es:", vacia)
+
